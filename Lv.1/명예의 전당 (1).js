076f0c8083e1d3034a39a1542b7293442df036ed
bear109 <jokerbear109@gmail.com>
2023-11-02 22:58:11 +0900

    solve: 명예의 전당 (1)

---
+++ Lv.1/명예의 전당 (1).js
@@ -0,0 +1,19 @@
+/**
+ * 문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12916
+ */
+function solution(k, score) {
+  var answer = [];
+  let arr = [];
+  for (let i = 0; i < score.length; i++) {
+    if (arr.length < k + 1) {
+      arr.push(score[i]);
+    }
+    arr.sort((a, b) => b - a);
+    if (arr.length >= k + 1) {
+      arr.pop();
+    }
+    answer.push(arr[arr.length - 1]);
+  }
+
+  return answer;
+}
