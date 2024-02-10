aafd144abd428c9717f80a740d883003cf458560
bear109 <jokerbear109@gmail.com>
2024-02-10 17:12:27 +0900

    solve: 최솟값 만들기

---
+++ Lv.2/최솟값 만들기.js
@@ -0,0 +1,12 @@
+/**
+ * 문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12941
+ */
+function solution(A, B) {
+  var answer = 0;
+  A.sort((a, b) => a - b);
+  B.sort((a, b) => b - a);
+  for (let i = 0; i < A.length; i++) {
+    answer += A[i] * B[i];
+  }
+  return answer;
+}
